ad8828c94b753b83ba61a3e1bb2ca8341f9935b2
guo chu <guochu604b@gmail.com>
2024-09-09 14:32:33 +0800

    minor changes

--- examples/LubaschBanuls2014b/table_iv_vmcbp.jl
+++ examples/LubaschBanuls2014b/table_iv_vmcbp.jl
@@ -55,7 +55,7 @@ function main(m::Int, n::Int, Dnew::Int, epoches::Int=100;D2::Int, D1::Int=2*D2^
 	ham = Heisenberg2D((m, n), J=0.25)
 
     for i in 1:epoches
-        @time train_loss, grad = parallel_energy_and_grad(ham, state, sampler, n_chain_per_rank=10, λ=1.0e-5)
+        @time train_loss, grad = parallel_energy_and_grad(ham, state, sampler, n_chain_per_rank=1, λ=1.0e-5)
 
         Optimise.update!(opt, x0, grad)
         state = re(x0)
